30dccc3bcc47e286900488870e80c97367a4a83f
Willie Soedewa <willie.soedewa12@gmail.com>
2022-08-15 15:05:30 +0800

    done

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,16 @@
+class QuestionsController < ApplicationController
+  def ask
+  end
+
+  def answer
+    @answers = ['Great!', 'Silly question, get dressed and go to work!', "I don't care, get dressed and go to work!"]
+
+    if params[:question].include? '?'
+      @answer = @answers[1]
+    elsif params[:question] == "I am going to work"
+      @answer = @answers[0]
+    else
+      @answer = @answers[2]
+    end
+  end
+end
